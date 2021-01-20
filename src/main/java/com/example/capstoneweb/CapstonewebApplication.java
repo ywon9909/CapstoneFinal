6d0c7c9950c7b8c10d0a8f6844cb0ea98d1e34b5
ywon9909@naver.com <ywon9909@naver.com>
2021-01-20 10:17:53 +0900

    commit

---
+++ src/main/java/com/example/capstoneweb/CapstonewebApplication.java
@@ -0,0 +1,13 @@
+package com.example.capstoneweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstonewebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapstonewebApplication.class, args);
+    }
+
+}
