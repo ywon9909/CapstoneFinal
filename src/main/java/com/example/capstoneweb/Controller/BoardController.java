d8a0da44537a77847720a94a5fe318824fdd1201
hahayoungyou <whgkdud0501@naver.com>
2021-01-22 16:46:11 +0900

    test

--- src/main/java/com/example/capstoneweb/Controller/BoardController.java
+++ src/main/java/com/example/capstoneweb/Controller/BoardController.java
@@ -1,6 +1,7 @@
 package com.example.capstoneweb.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.capstoneweb.model.Board;
@@ -25,4 +26,19 @@ public class BoardController {
     public Board createBoard(@RequestBody Board board) {
         return boardService.createBoard(board);
     }
+    @GetMapping("/board/{num}")
+    public ResponseEntity<Board> getBoardByNum(
+            @PathVariable Integer num){
+        return boardService.getBoard(num);
+    }
+    // update board
+    @PutMapping("/board/{no}")
+    public ResponseEntity<Board> updateBoardByNo(
+            @PathVariable Integer no, @RequestBody Board board){
+
+        return boardService.updateBoard(no, board);
+    }
+
+
+
 }
--- src/main/java/com/example/capstoneweb/service/BoardService.java
+++ src/main/java/com/example/capstoneweb/service/BoardService.java
@@ -1,8 +1,10 @@
 package com.example.capstoneweb.service;
 
+import com.example.capstoneweb.exception.ResourceNotFoundException;
 import com.example.capstoneweb.model.Board;
 import com.example.capstoneweb.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,5 +20,21 @@ public class BoardService {
     }
     public Board createBoard(Board board) {
         return boardRepository.save(board);
+    }
+    public ResponseEntity<Board> getBoard(Integer num){
+        Board board = boardRepository.findById(num).orElseThrow(()-> new ResourceNotFoundException("Not exist Board Data by no : ["+num+"]"));
+
+        return ResponseEntity.ok(board);
+    }
+    public ResponseEntity<Board> updateBoard(
+            Integer no, Board updatedBoard) {
+        Board board = boardRepository.findById(no)
+                .orElseThrow(() -> new ResourceNotFoundException("Not exist Board Data by no : ["+no+"]"));
+        board.setTitle(updatedBoard.getTitle());
+        board.setQuestion(updatedBoard.getQuestion());
+
+
+        Board endUpdatedBoard = boardRepository.save(board);
+        return ResponseEntity.ok(endUpdatedBoard);
     }
 }
