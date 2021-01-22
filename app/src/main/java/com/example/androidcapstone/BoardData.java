1772cb0ef003f058f3c81cea4ca9f8276eecdfb3
10000bit <khb990507@naver.com>
2021-01-22 11:55:30 +0900

    retrofit success

--- app/src/main/java/com/example/androidcapstone/BoardData.java
+++ app/src/main/java/com/example/androidcapstone/BoardData.java
@@ -1,6 +1,106 @@
 package com.example.androidcapstone;
 
+import com.google.gson.annotations.SerializedName;
+
 public class BoardData {
+
+    Integer num;
+
+
+    String title;
+    String question;
+    String answer1;
+
+    String answer2;
+    String answer3;
+    String answer4;
+    String answer5;
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer1() {
+        return answer1;
+    }
+
+    public void setAnswer1(String answer1) {
+        this.answer1 = answer1;
+    }
+
+    public String getAnswer2() {
+        return answer2;
+    }
+
+    public void setAnswer2(String answer2) {
+        this.answer2 = answer2;
+    }
+
+    public String getAnswer3() {
+        return answer3;
+    }
+
+    public void setAnswer3(String answer3) {
+        this.answer3 = answer3;
+    }
+
+    public String getAnswer4() {
+        return answer4;
+    }
+
+    public void setAnswer4(String answer4) {
+        this.answer4 = answer4;
+    }
+
+    public String getAnswer5() {
+        return answer5;
+    }
+
+    public void setAnswer5(String answer5) {
+        this.answer5 = answer5;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Board{" +
+                "num=" + num +
+                ", title='" + title + '\'' +
+                ", question='" + question + '\'' +
+                ", answer1='" + answer1 + '\'' +
+                ", answer2='" + answer2 + '\'' +
+                ", answer3='" + answer3 + '\'' +
+                ", answer4='" + answer4 + '\'' +
+                ", answer5='" + answer5 + '\'' +
+                '}';
+    }
+
+
+
+
+
+    /*
     private int num;
     private String title;
     private String question;
@@ -11,26 +111,32 @@ public class BoardData {
     public int getNum() {
         return num;
     }
+    public int setNum() { return this.num = num; }
 
     public String getTitle() {
         return title;
     }
+    public String setTitle() { return this.title = title; }
 
     public String getQuestion() {
         return question;
     }
+    public String setQuestion() { return this.question = question; }
 
     public String getAnswer1() {
         return answer1;
     }
+    public String setAnswer1() { return this.answer1 = answer1; }
 
     public String getAnswer2() {
         return answer2;
     }
+    public String setAnswer2() { return this.answer2 = answer2; }
 
     public String getAnswer3() {
         return answer3;
     }
+    public String setAnswer3() { return this.answer3 = answer3; }
 
     @Override
     public String toString() {
@@ -43,4 +149,6 @@ public class BoardData {
                 ", answer3='" + answer3 + '\'' +
                 '}';
     }
+
+     */
 }
