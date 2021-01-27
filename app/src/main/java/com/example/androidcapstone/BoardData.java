f4a4534972e4450700ab02343dd0f7fc679aa019
10000bit <khb990507@naver.com>
2021-01-27 15:37:44 +0900

    DB 새로 받아오기

--- app/src/main/java/com/example/androidcapstone/BoardData.java
+++ app/src/main/java/com/example/androidcapstone/BoardData.java
@@ -2,25 +2,24 @@ package com.example.androidcapstone;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.sql.Time;
+
 public class BoardData {
 
-    Integer num;
-
+    Integer board_no;
     String title;
     String question;
-    String answer1;
+    Time board_date;
+    Integer board_like;
+    String category;
+    String board_id;
 
-    String answer2;
-    String answer3;
-    String answer4;
-    String answer5;
-
-    public Integer getNum() {
-        return num;
+    public Integer getBoard_no() {
+        return board_no;
     }
 
-    public void setNum(Integer num) {
-        this.num = num;
+    public void setBoard_no(Integer board_no) {
+        this.board_no = board_no;
     }
 
     public String getTitle() {
@@ -39,44 +38,36 @@ public class BoardData {
         this.question = question;
     }
 
-    public String getAnswer1() {
-        return answer1;
+    public Time getBoard_date() {
+        return board_date;
     }
 
-    public void setAnswer1(String answer1) {
-        this.answer1 = answer1;
+    public void setBoard_date(Time board_date) {
+        this.board_date = board_date;
     }
 
-    public String getAnswer2() {
-        return answer2;
+    public Integer getBoard_like() {
+        return board_like;
     }
 
-    public void setAnswer2(String answer2) {
-        this.answer2 = answer2;
+    public void setBoard_like(Integer board_like) {
+        this.board_like = board_like;
     }
 
-    public String getAnswer3() {
-        return answer3;
+    public String getCategory() {
+        return category;
     }
 
-    public void setAnswer3(String answer3) {
-        this.answer3 = answer3;
+    public void setCategory(String category) {
+        this.category = category;
     }
 
-    public String getAnswer4() {
-        return answer4;
+    public String getBoard_id() {
+        return board_id;
     }
 
-    public void setAnswer4(String answer4) {
-        this.answer4 = answer4;
-    }
-
-    public String getAnswer5() {
-        return answer5;
-    }
-
-    public void setAnswer5(String answer5) {
-        this.answer5 = answer5;
+    public void setBoard_id(String board_id) {
+        this.board_id = board_id;
     }
 
 
@@ -84,70 +75,14 @@ public class BoardData {
     @Override
     public String toString() {
         return "Board{" +
-                "num=" + num +
+                "board_no=" + board_no +
                 ", title='" + title + '\'' +
                 ", question='" + question + '\'' +
-                ", answer1='" + answer1 + '\'' +
-                ", answer2='" + answer2 + '\'' +
-                ", answer3='" + answer3 + '\'' +
-                ", answer4='" + answer4 + '\'' +
-                ", answer5='" + answer5 + '\'' +
+                ", board_date='" + board_date + '\'' +
+                ", board_like='" + board_like + '\'' +
+                ", category='" + category + '\'' +
+                ", id='" + board_id + '\'' +
                 '}';
     }
 
-
-
-
-
-    /*
-    private int num;
-    private String title;
-    private String question;
-    private String answer1;
-    private String answer2;
-    private String answer3;
-
-    public int getNum() {
-        return num;
-    }
-    public int setNum() { return this.num = num; }
-
-    public String getTitle() {
-        return title;
-    }
-    public String setTitle() { return this.title = title; }
-
-    public String getQuestion() {
-        return question;
-    }
-    public String setQuestion() { return this.question = question; }
-
-    public String getAnswer1() {
-        return answer1;
-    }
-    public String setAnswer1() { return this.answer1 = answer1; }
-
-    public String getAnswer2() {
-        return answer2;
-    }
-    public String setAnswer2() { return this.answer2 = answer2; }
-
-    public String getAnswer3() {
-        return answer3;
-    }
-    public String setAnswer3() { return this.answer3 = answer3; }
-
-    @Override
-    public String toString() {
-        return "BoardData{" +
-                "num=" + num +
-                ", title='" + title + '\'' +
-                ", question='" + question + '\'' +
-                ", answer1='" + answer1 + '\'' +
-                ", answer2='" + answer2 + '\'' +
-                ", answer3='" + answer3 + '\'' +
-                '}';
-    }
-
-     */
 }
