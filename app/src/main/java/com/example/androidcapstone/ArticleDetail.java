c6731d6ce401cf54b1f1556e6b4ac7a5c34feea0
10000bit <khb990507@naver.com>
2021-02-01 12:25:15 +0900

    commit

--- app/src/main/java/com/example/androidcapstone/ArticleDetail.java
+++ app/src/main/java/com/example/androidcapstone/ArticleDetail.java
@@ -13,23 +13,14 @@ import org.w3c.dom.Text;
 
 public class ArticleDetail extends AppCompatActivity {
     // 글 목록의 아이템을 누르면 나오는 글 상세 화면
-    ActivityArticleDetailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_detail);
 
-        TextView title = binding.title;
-        TextView question = binding.question;
-        TextView answer1 = binding.answer1;
-        TextView answer2 = binding.answer2;
-        TextView answer3 = binding.answer3;
-        TextView answer4 = binding.answer4;
-        TextView answer5 = binding.answer5;
-
-        //TextView title = (TextView)findViewById(R.id.title);
-        //TextView question = (TextView)findViewById(R.id.question);
+        TextView title = (TextView)findViewById(R.id.title);
+        TextView question = (TextView)findViewById(R.id.question);
 
         Intent intent = getIntent();
 
