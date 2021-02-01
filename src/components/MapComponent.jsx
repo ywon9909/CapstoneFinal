c917002e9c6c59f13532251da0f76b67b5aad011
hahayoungyou <whgkdud0501@naver.com>
2021-02-01 14:03:55 +0900

    위치표시

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -25,6 +25,49 @@ class MapComponent extends Component {
                   level: 7
                 };
                 const map = new window.kakao.maps.Map(container, options);
+
+                if (navigator.geolocation) {
+                    // GeoLocation을 이용해서 접속 위치를 얻어옵니다.
+                    navigator.geolocation.getCurrentPosition(function(position){
+                        
+                        var lat = position.coords.latitude, // 위도
+                            lon = position.coords.longitude; // 경도
+                            
+                        var locPostion = new kakao.maps.LatLng(lat, lon), //마커가 표시될 위치를 geolocation 좌표로 생성합니다.
+                            message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다.
+            
+                        // 마커와 인포윈도우를 표시합니다.
+                        displayMarker(locPostion,message);
+                    });
+                } else {   
+            
+                }
+                
+                // 지도에 마커와 인포윈도우를 표시하는 함수입니다.
+                function displayMarker(locPostion, message) {
+                    
+                    // 마커를 생성합니다.
+                    var marker = new kakao.maps.Marker({
+                        map: map,
+                        position: locPostion
+                    });
+            
+                    var iwContent = message, // 인포윈도우에 표시할 내용
+                        iwRemoveable = true;
+                    
+                    // 인포윈도우를 생성합니다.
+                    var infowindow = new kakao.maps.InfoWindow({
+                        content : iwContent,
+                        removable : iwRemoveable
+                    });
+            
+                    // 인포윈도우를 마커위에 표시합니다.
+                    infowindow.open(map,marker);
+            
+                    // 지도 중심좌표를 접속위치로 변경합니다.
+                    map.setCenter(locPostion);
+                }
+            
               });
         };
     }
