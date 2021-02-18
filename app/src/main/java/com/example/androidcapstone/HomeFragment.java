5c2422eea7222b6ebadb9571ee0fd0a3db41f1a9
10000bit <khb990507@naver.com>
2021-02-18 17:41:32 +0900

    board search

--- app/src/main/java/com/example/androidcapstone/HomeFragment.java
+++ app/src/main/java/com/example/androidcapstone/HomeFragment.java
@@ -3,10 +3,13 @@ package com.example.androidcapstone;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class HomeFragment extends Fragment {
@@ -16,6 +19,29 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+
+        Button button=(Button)view.findViewById(R.id.buttonsearch);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                // Create new fragment and transaction
+                Fragment newFragment = new SearchFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+
+                // Replace whatever is in the fragment_container view with this fragment,
+                // and add the transaction to the back stack
+                transaction.replace(R.id.main_layout, newFragment);
+                transaction.addToBackStack(null);
+
+                // Commit the transaction
+                transaction.commit();
+            }
+        });
+
+
+        return view;
     }
 }
