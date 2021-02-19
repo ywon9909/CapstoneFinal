64582ef2539ef3b3f892df9721138c9daeffd739
10000bit <khb990507@naver.com>
2021-02-19 16:41:39 +0900

    map Rest API start

--- app/src/main/java/com/example/androidcapstone/JsonApi.java
+++ app/src/main/java/com/example/androidcapstone/JsonApi.java
@@ -12,37 +12,41 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface JsonApi {
-    //@GET("/api/mobile/board?category=성형외과")
-    //Call<List<BoardData>> getBoard();
-
+    // 글 조회
     @GET("/api/mobile/board")
     Call<List<BoardData>> getBoard(@Query("category") String category);
 
+    // 글 등록
     @POST("/api/board")
     Call<BoardData> addPost(@Body BoardData boardData);
 
+    // 글 수정
     @PUT("/api/board/{no}")
     Call<Void> updatePost(@Path("no") Integer no, @Body BoardData boardData);
 
+    // 글 삭제
     @DELETE("/api/board/{no}")
     Call<Void> deleteUser(@Path("no") int no);
 
+    // 글 검색
     @GET("/api/board/search/{keyword}/{searchType}")
     Call<List<BoardData>> getSearchBoards(@Path(value = "keyword") String keyword,
                                           @Path(value = "searchType") String searchType);
 
-
-    //@GET("/api/board/comment/1")
-    //Call<List<CommentData>> getComment();
-
-
+    // 댓글 조회
     @GET("/api/board/comment/{num}")
     Call<List<CommentData>> getComment(@Path("num") Integer num);
 
+    // 댓글 등록
     @POST("api/board/comment")
     Call<CommentData> addComment(@Body CommentData commentData);
 
+    // 댓글 삭제
     @DELETE("/api/board/comment/{no}")
     Call<Void> deleteComment(@Path("no") int no);
+
+    // 지도
+    @GET("/v2/local/search/keyword.json?{format}")
+    Call<List<MapData>> getMap(@Query("format") String format);
 
 }
